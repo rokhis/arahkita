0960d39c8bea1195910ab8cf80cda33d7f793d67
Gusde Biyan Ambarista <gusdebiyan28@gmail.com>
2022-03-28 00:59:08 +0700

    revisi

--- Arah_kita_web/resources/views/login/index.blade.php
+++ Arah_kita_web/resources/views/login/index.blade.php
@@ -1,48 +1,61 @@
 @extends('layout.main')
 @section('title', 'Arah Kita | Login')
 @section('container')
-    <div class="container">
-      <div class="row justify-content-center mt-5">
-        <div class="col-lg-5">
-
-          @if(session()->has('success'))
-              <div class="alert alert-success alert-dismissible fade show" role="alert">
-              {{ session('success') }}
-              </div>
-          @endif
-
-          @if(session()->has('loginError'))
-              <div class="alert alert-danger alert-dismissible fade show" role="alert">
-              {{ session('loginError') }}
-              </div>
-          @endif
-          <main class="form-signin">
-            {{-- <img class="mb-4" src="/img/logo.png" alt="" width="150" height="100"> --}}
-            <h1 class="h3 text-black mb-3 fw-normal text-center ">Silahkan Login</h1>
-
-            <form action="/login" method="POST" class="form-control rounded">
-              @csrf
-
-              <div class="col-md mb-2">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" required value="{{ old('email') }}" autofocus>
-                @error('email')
-                <div class="invalid-feedback">
-                  {{ $message }}
-                </div>
-              @enderror
-              </div>
-              <div class="col-md">
-                <label for="password" class="col-form-label">Password</label>
-                  <input type="password" id="password" class="form-control" name="password" class="form-control" aria-describedby="passwordHelpInline" placeholder="password" required>
-              </div>
-              
-              <button class="w-100 btn btn-md btn-primary mt-3" type="submit">Login</button>
-              <small class="mt-3 text-right text-black">Belum punya akun ? <a href="/register"> Daftar</a></small>
-            </form>
-          </main>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="text-center">
+        <img class="imgLogin"  src="/img/logo.png" width="470" height="356" alt="" > 
       </div>
-
     </div>
+    
+    <div class="col">
+      
+      <main class="">
+        
+        <div class="layout">
+          <div class="card mt-5" style="width: 25rem;">
+            <form action="/login" method="POST" class="form-signin">
+              @csrf
+                        {{-- sesion --}}
+                        @if(session()->has('success'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('success') }}
+                                </div>
+                        @endif
+                        @if(session()->has('loginError'))
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                              {{ session('loginError') }}
+                          </div>
+                        @endif
+                    
+                <div class="col-md mb-2">
+                      <label for="email" class="form-label">Email</label>
+                      <input type="email" id="email" class="form-control form-control-sm bg-light @error('email') is-invalid @enderror" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" required value="{{ old('email') }}" autofocus>
+                      @error('email')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                </div>
+    
+                <div class="col-md mb-2">
+                  <label for="password" class="col-form-label">Password</label>
+                  <input type="password" id="password" class="form-control form-control-sm bg-light" name="password" class="form-control" aria-describedby="passwordHelpInline" placeholder="password" required>
+                </div>
+                
+                <div class="text-center mb-3">
+                  <button class="w-50 btn btn-sm text-white mt-3" type="submit">Login</button>
+                </div>
+                <small class=" text-right text-black">Belum punya akun ? <a href="/register"> Daftar</a></small>
+              
+              </form>
+            </div>
+          </div>    
+        </main>
+    </div>
+  
+  </div>
+</div>
     
  @endsection
--- Arah_kita_web/resources/views/register/index.blade.php
+++ Arah_kita_web/resources/views/register/index.blade.php
@@ -2,61 +2,72 @@
 @section('title', 'Arah Kita | Register')
 @section('container')
     <div class="container">
-      <div class="row justify-content-center mt-5">
-        <div class="col-lg-5">
-          <main class="">
-            <form action="/register" method="POST" class="form-control form-control-sm">
-              @csrf
-              <h3 class="text-white mb-3 fw-normal ">Silahkan Daftar</h3>
-              
+      <div class="row justify-content-center">
+          <div class="card border-0 mt-5" style="max-width: 800px;">
+            <div class="row"> 
+              <div class="col border  text-center ">
+                <img src="/img/regist-logo.png" class="img-fluid logo2 " width="200" height="200">
+              </div>
+              <div class="col-md-7 ">
+                <main class="">
+                  <form action="/register" method="POST" class="form-register" >
+                    @csrf
+                    <h3 class="text-white mb-3 fw-normal ">Silahkan Daftar</h3>
+                    
+                      <div class="col-auto mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nama</label>
+                        <input type="text" class="form-control form-control-sm @error('nama') is-invalid @enderror" name="nama" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nama" required value="{{ old('nama') }}">
+                      @error('nama')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                      </div>
+                    
+                      <div class="col-auto mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Username</label>
+                        <input type="text" class="form-control form-control-sm @error('username') is-invalid @enderror" name="username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username" required value="{{ old('username') }}">
+                        @error('username')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                      </div>
+      
+                      <div class="col-auto mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" id="email" class="form-control form-control-sm @error('email') is-invalid @enderror" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" required value="{{ old('email') }}">
+                        @error('email')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                      </div>
+      
+                     <div class="col-sm-5">
+                        <label for="password" class="col-form-label">Password</label>
+                        <input type="password" id="password" class="form-control form-control-sm @error('password') is-invalid @enderror" name="password" class="form-control" aria-describedby="passwordHelpInline" placeholder="password" required>
+                        @error('password')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                        @enderror
+                        <span id="password" class="form-text">
+                          Must be 8-20 characters long.
+                         </span>
+                     </div>
+                        
+                    <button class="w-100 btn btn-md text-white mt-3" type="submit">Register</button>
+                    <small class="mt-5 text-right">Sudah Punya Akun ? <a href="/login"> Login</a></small>
+                  </form>
+                </main>
+  
+              </div>
 
-                <div class="col-auto mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Nama</label>
-                  <input type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nama" required value="{{ old('nama') }}">
-                @error('nama')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                @enderror
-                </div>
-              
-                <div class="col-auto mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Username</label>
-                  <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username" required value="{{ old('username') }}">
-                  @error('username')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                @enderror
-                </div>
+            </div>
 
-                <div class="col-auto mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <input type="email" id="email" class="form-control  @error('email') is-invalid @enderror" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" required value="{{ old('email') }}">
-                  @error('email')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                @enderror
-                </div>
-
-               <div class="col-sm-5">
-                  <label for="password" class="col-form-label">Password</label>
-                  <input type="password" id="password" class="form-control  @error('password') is-invalid @enderror" name="password" class="form-control" aria-describedby="passwordHelpInline" placeholder="password" required>
-                  @error('password')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                @enderror
-                  <span id="password" class="form-text">
-                    Must be 8-20 characters long.
-                   </span>
-               </div>
-                  
-              <button class="w-100 btn btn-md btn-primary mt-3" type="submit">Register</button>
-              <small class="mt-5 text-right">Sudah Punya Akun ? <a href="/login"> Login</a></small>
-            </form>
-          </main>
+          </div>
+                 
       </div>
       </div>
     </div>
